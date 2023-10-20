dc0fa5efd17ec1f315f0dd326152c617d82aa2ee
Aman Kapil <77871952+Amankapil@users.noreply.github.com>
2023-10-20 15:04:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const secretKey = crypto.randomBytes(32).toString("hex");
 import session from "express-session";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7700;
 
 app.use(express.json());
 app.use(cors());
